d6e755ca9b12e69dcda0749f4933206a5b242ac8
Valensja <walentynaostrowska2@gmail.com>
2022-09-13 11:41:51 +0200

    Week

--- Walentyna.php
+++ Walentyna.php
@@ -1,2 +1,17 @@
 <?php
 echo"Hello";
+?>
+
+<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Application</title>
+		</head>
+		<body>
+            <ul>
+                <li>Monday</li>
+                <li>Tuersday</li>
+                <li>Wednesday</li>
+            </ul>
+</body>
