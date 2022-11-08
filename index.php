a3d90361e9a214ad8df39d47f296b60b4ce2d0a9
Sav Eklund <sav_eklund@hotmail.com>
2022-11-08 10:32:25 +0100

    updated index and SavRabbit

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$rabbit = new AnnaRabbit();
-$feeling = $rabbit->getRabbitFeeling(true, false);
-$rabbit->printRabbit($feeling);
+//$rabbit = new AnnaRabbit();
+//$feeling = $rabbit->getRabbitFeeling(true, false);
+//$rabbit->printRabbit($feeling);
 
 // Carina
 
@@ -13,6 +13,13 @@ $rabbit->printRabbit($feeling);
 
 // Sav
 
+$rabbit = new SavRabbit();
+$feeling = $rabbit->getRabbitFeeling(false, true);
+$rabbit->printRabbit($feeling);
 
 
 
+//true/false = happy
+//true/true = happy
+//false/false = sad
+//false/true = angry 
--- src/SavRabbit.php
+++ src/SavRabbit.php
@@ -1,41 +1,44 @@
 <?php
+
+
 declare(strict_types=1);
 
-function getRabbitFurColor(bool $sun, bool $wind) : string {
-    $rabbitFur = "red";
-    if($sun == true) {
-        $rabbitFur = "green";
-    } 
-    else if($wind == false) {
-        $rabbitFur = "blue";  
-    }
-    return $rabbitFur;
+class SavRabbit {
+
+        private function getRabbitFurColor(bool $sun, bool $wind) : string {
+            $rabbitFur = "red";
+            if($sun == true) {
+                $rabbitFur = "green";
+            } 
+            else if($wind == false) {
+                $rabbitFur = "blue";  
+            }
+            return $rabbitFur;
+        }
+
+        public function getRabbitFeeling(bool $sun, bool $wind) : string {
+            $rabbitColor = $this->getRabbitFurColor($sun, $wind);
+
+            switch($rabbitColor) {
+                case "red":
+                    $rabbitFeeling="angry";
+                    break;
+            
+                case "blue":
+                    $rabbitFeeling="sad";
+                    break;
+            
+                case "green":
+                    $rabbitFeeling="happy";
+                    break;
+            }
+            return $rabbitFeeling;
+        }
+
+        public function printRabbit($m) {
+            echo "Haren känner sig " . $m . ".";    
+        }
+
 }
-
-function getRabbitFeeling(bool $sun, bool $wind) : string {
-    $rabbitColor = getRabbitFurColor($sun, $wind);
-
-    switch($rabbitColor) {
-        case "red":
-            $rabbitFeeling="angry";
-             break;
-    
-        case "blue":
-            $rabbitFeeling="sad";
-            break;
-    
-        case "green":
-            $rabbitFeeling="happy";
-            break;
-    }
-    return $rabbitFeeling;
-}
-
-function printRabbit($m) {
-    echo "Haren känner sig " . $m . ".";    
-}
-
-$feeling = getRabbitFeeling(true, false);
-printRabbit($feeling);
 
 ?>
