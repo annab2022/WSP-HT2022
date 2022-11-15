a2971ede7802c8d67a598251e94c6d1bcc94ab2c
Carina <cariiinaav@gmail.com>
2022-11-15 12:00:50 +0100

    Added SuperHelper

---
+++ src/SuperHelper.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+class SuperHelper {
+    public function isHardVocal(string $vocal): bool {
+        return true;
+    }
+
+    public function countString(string $sign): int {
+        return 1;
+    } 
+}
+
+
+?>
