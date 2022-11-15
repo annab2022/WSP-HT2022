24e4c7ce41800b7615e29200fb0ce332a3e12671
Valensja <walentynaostrowska2@gmail.com>
2022-11-15 12:40:10 +0100

    Added metod countString

--- tests/SuperHelperTest.php
+++ tests/SuperHelperTest.php
@@ -14,10 +14,35 @@ final class SuperHelperTest extends TestCase {
 
         $this->vocal = new SuperHelper();
         $this->sign = new SuperHelper();
-
     }
 
+    public function test_getCountString_shouldReturn_when_string(): void {
+        $this->markTestSkipped("Hello");
 
+        $result = $this->sign->countString('ghjk');
 
-    
+        $this->assertEquals(4, $result);
+    }
+    public function test_getCountString_shouldReturn_when_emptyString(): void {
+
+        $result = $this->sign->countString('');
+
+        $this->assertEquals(0, $result);
+    }
+    public function test_getCountString_shouldReturn_when_NULLString(): void {
+
+        $result = $this->sign->countString('NULL');
+
+        $this->assertEquals(NULL, $result);
+    }
+    public function test_getCountString_shouldReturn_when_longString(): void {
+
+        $result = $this->sign->countString('fghjklöähjklö234567..-');
+
+        $this->assertEquals(22, $result);
+    }
 }
+
+/*' '/ 0
+NULL/ 0
+fghjklöähjklö234567..-/ 22*/
